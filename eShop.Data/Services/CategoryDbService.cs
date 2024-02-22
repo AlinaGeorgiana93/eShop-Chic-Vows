05d310ae427cb51fcc4eebe2a86e82536328dd57
Georgis93 <georgiana.alina93@yahoo.com>
2024-02-22 16:27:14 +0100

    Add project files.

---
+++ eShop.Data/Services/CategoryDbService.cs
@@ -0,0 +1,17 @@
+﻿
+using eShop.API.DTO;
+using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Data.Services;
+
+public class CategoryDbService(EShopContext db, IMapper mapper) : DbService(db, mapper)
+{
+    public override async Task<List<TDto>> GetAsync<TEntity, TDto>()
+    {
+        //IncludeNavigationsFor<Filter>();
+        //IncludeNavigationsFor<Product>();
+        return await base.GetAsync<TEntity, TDto>();
+    }
+
+}
